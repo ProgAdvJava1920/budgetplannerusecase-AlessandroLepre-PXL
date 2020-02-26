0268b69e3aa8a2d3d1665f9053e4142bbee28a7e
Alessandro <alessandro.lepre@student.pxl.be>
2020-02-26 11:39:50 +0100

    Testmap toegevoegd

---
+++ src/main/test/java/be/pxl/student/util/BudgetPlannerImporterTest.java
@@ -0,0 +1,12 @@
+package be.pxl.student.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BudgetPlannerImporterTest {
+
+    @Test
+    void deserialise() {
+    }
+}
